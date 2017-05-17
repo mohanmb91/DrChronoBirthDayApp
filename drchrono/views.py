ab10cc81560b49421eab1e93a5113fdcfb4edc71
mohanmb91 <mohankumarmbuniv@gmail.com>
2017-05-17 03:25:46 -0700

    got the patients data

--- drchrono/views.py
+++ drchrono/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
 import datetime, requests
+import settings;
 
 
 def get_code(request):
     code = "";
+
     code = request.GET.get('code', 'NotFound');
     template = "index.html";
 
@@ -20,6 +22,7 @@ def get_code(request):
         data = response.json()
 
         access_token = data['access_token']
+        settings.ACCESS_TOKEN = access_token;
         refresh_token = data['refresh_token']
         context = {'codeKey': code, 'access_token': access_token, 'refresh_token': refresh_token};
 
@@ -29,9 +32,22 @@ def get_code(request):
         response.raise_for_status()
         data = response.json()
 
+
         # You can store this in your database along with the tokens
         username = data['username']
         print username;
+
+        headers = {
+            'Authorization': 'Bearer %s' % access_token,
+        }
+
+        patients = []
+        patients_url = 'https://drchrono.com/api/patients_summary'
+        while patients_url:
+            data = requests.get(patients_url, headers=headers).json()
+            patients.extend(data['results'])
+            patients_url = data['next']  # A JSON null on the last page
+        print patients;
         return render(request, template, context);
     else:
         context = {'codeKey': code};
